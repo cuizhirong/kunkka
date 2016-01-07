7848766705243500517bd1b6e3093a03ba8d5b07
Lee Yao <yaoli@unitedstack.com>
2016-01-07 16:44:33 +0800

    complete dashboard layout
    Change-Id: I47b664089319b2fe5217077c4eb8b9e30c4d7e09
    Type: NF
    Jira: HALO-70

--- client/components/main_table/index.jsx
+++ client/components/main_table/index.jsx
@@ -1,7 +1,9 @@
 require('./style/index.less');
 
 var React = require('react');
-var Table = require('client/uskin/index').Table;
+var uskin = require('client/uskin/index');
+var Table = uskin.Table;
+var Button = uskin.Button;
 
 class MainTable extends React.Component {
 
@@ -12,8 +14,22 @@ class MainTable extends React.Component {
   render() {
     var props = this.props;
 
+    function listener() {}
+
     return (
-      <Table column={props.column} data={props.data} dataKey={props.dataKey} />
+      <div className="halo-main-table">
+        <div className="header">
+          <h3>Instances</h3>
+        </div>
+        <div className="operation-list">
+          <Button value="Initial" initial={true} onClick={listener} iconClass="glyphicon icon-region"/>
+          <Button value="Initial" type="create" initial={true} onClick={listener} iconClass="glyphicon icon-create"/>
+          <Button value="Initial" type="delete" initial={true} onClick={listener} iconClass="glyphicon icon-more"/>
+          <Button value="Initial" type="cancel" initial={true} onClick={listener} iconClass="glyphicon icon-edit"/>
+          <Button value="Initial" type="cancel" initial={true} disabled={true} onClick={listener} iconClass="glyphicon icon-disable"/>
+        </div>
+        <Table column={props.column} data={props.data} dataKey={props.dataKey} />
+      </div>
     );
   }
 
