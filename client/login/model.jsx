2e5721dbbaa212cc3c5459bc30932bf16baacc19
yiqun <yiqun@unitedstack.com>
2016-01-14 10:58:04 +0800

    optimize login
    Change-Id: I466deb1e1860c199d4973b0d2ea1802f189e7ecc
    Type: BF
    Jira: HALO-82

--- client/login/model.jsx
+++ client/login/model.jsx
@@ -7,7 +7,10 @@ class Model extends React.Component {
     super(props);
 
     this.state = {
-      loginError: false
+      loginError: false,
+      usernameEmptyError: false,
+      pwdEmptyError: false,
+      isSubmitting: false
     };
 
     this.doSubmit = this.doSubmit.bind(this);
@@ -16,22 +19,53 @@ class Model extends React.Component {
   doSubmit(e) {
     e.preventDefault();
 
-    var refs = this.refs,
-      that = this;
+    if (this.state.isSubmitting) {
+      return;
+    }
 
+    var refs = this.refs,
+      that = this,
+      username = refs.username.value,
+      pwd = refs.pwd.value;
+
+    that.setState({
+      loginError: false,
+      usernameEmptyError: false,
+      pwdEmptyError: false
+    });
+
+    var isEmpty = !username || !pwd;
+    if (isEmpty) {
+      if (!username) {
+        that.setState({
+          usernameEmptyError: true
+        });
+      }
+      if (!pwd) {
+        that.setState({
+          pwdEmptyError: true
+        });
+      }
+      return;
+    }
+
+    that.setState({
+      isSubmitting: true
+    });
     request.post({
       url: '/auth/login',
       dataType: 'json',
       contentType: 'application/json',
       data: {
-        username: refs.username.value,
-        password: refs.pwd.value
+        username: username,
+        password: pwd
       }
     }).then(function(data) {
       window.location = '/';
     }, function(err) {
       that.setState({
-        loginError: true
+        loginError: true,
+        isSubmitting: false
       });
     });
 
@@ -43,8 +77,8 @@ class Model extends React.Component {
 
     return (
       <form method="POST" onSubmit={this.doSubmit}>
-        <input type="text" ref="username" placeholder={props.accountPlaceholder} autoFocus="autofocus" autoComplete="off" />
-        <input type="password" ref="pwd" placeholder={props.pwdPlaceholder} autoComplete="off" />
+        <input type="text" ref="username" className={state.usernameEmptyError ? 'error' : ''} placeholder={props.accountPlaceholder} autoFocus="autofocus" autoComplete="off" />
+        <input type="password" ref="pwd" className={state.pwdEmptyError ? 'error' : ''} placeholder={props.pwdPlaceholder} autoComplete="off" />
         <div className="tip-wrapper">
           <div className={'input-error' + (state.loginError ? '' : ' hide')}>
             <i className="glyphicon icon-status-warning"></i><span>{props.errorTip}</span>
