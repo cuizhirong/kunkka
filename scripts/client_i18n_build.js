a7277ba83acd1e4586db401fbda8e6dd100d1fcc
Lee Yao <yaoli@unitedstack.com>
2016-01-23 18:18:41 +0800

    refine client i18n transpiler logic
    Change-Id: I0245875ba1e19ff938aa8185dd89af0b8cd9b875
    Type: NF
    Jira: HALO-104

--- scripts/client_i18n_build.js
+++ scripts/client_i18n_build.js
@@ -1,26 +1,47 @@
 var fs = require('fs');
 var path = require('path');
-var baseLang = require('../i18n/shared/lang.json');
 var glob = require('glob');
+var chalk = require('chalk');
 
 var language = process.env.npm_config_lang || process.env.language;
 if (!language) {
   language = 'zh-CN';
 }
 
-if (!language) {
-  language = 'zh-CN';
-}
 var rootDir = path.resolve(__dirname, '..');
 
-var output = baseLang[language];
-
 glob(rootDir + '/client/dashboard/modules/**/lang.json', {}, function(er, files) {
-  for (var i = 0, len = files.length; i < len; i++) {
-    Object.assign(output, require(files[i])[language]);
+  files.unshift('../i18n/shared/lang.json');
+  var file = '';
+  try {
+    var output = {};
+    for (var i = 0, len = files.length; i < len; i++) {
+      file = files[i];
+      assign(output, require(files[i])[language], files[i]);
+    }
+    writeFile(JSON.stringify(output));
+  } catch (e) {
+    console.log(chalk.white.bgRed.bold(' ERROR ') + ' i18n file ' + file + ' got something wrong!');
   }
-  writeFile(JSON.stringify(output));
 });
+
+function assign(source, obj, path) {
+  var k = null;
+  Object.keys(obj).forEach(function(m) {
+    var b = Object.keys(source).some(function(n) {
+      if (m === n) {
+        k = n;
+        return true;
+      }
+      return false;
+    });
+    if (b) {
+      console.log(chalk.white.bgYellow.bold(' WARNING ') + ' the key ' + chalk.bold(k) + ' is duplicate in the file ' + path);
+    } else {
+      source[m] = obj[m];
+    }
+  });
+}
 
 function writeFile(str) {
   var dir = path.resolve(rootDir, 'i18n/client');
@@ -30,4 +51,5 @@ function writeFile(str) {
     fs.mkdirSync(dir);
   }
   fs.writeFileSync(path.join(dir, 'lang.json'), str, 'utf-8');
+  console.log(chalk.white.bgGreen.bold(' SUCCESS ') + ' The ' + chalk.bold(language) + ' i18n file has been generated!');
 }
