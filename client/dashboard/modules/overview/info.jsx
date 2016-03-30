315b2aff0982c005f0ac9153d338ae40c8ae7332
xuyina <xuyina@unitedstack.com>
2016-03-30 17:12:51 +0800

    add overview page
    Change-Id: Ice168a64e42289041df98432fa22c4160a722177
    Type: NF
    Jira: HALO-191

---
+++ client/dashboard/modules/overview/info.jsx
@@ -0,0 +1,89 @@
+require('./style/index.less');
+
+var React = require('react');
+
+var __ = require('i18n/client/lang.json');
+
+class ResourceInfo extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    var info = [{
+      title: __.instance,
+      key: 'instances',
+      link: 'instance'
+    }, {
+      title: __.volume,
+      key: 'volumes',
+      link: 'volume'
+    }, {
+      title: __.router,
+      key: 'router',
+      link: 'volume'
+    }, {
+      title: __['floating-ip'],
+      key: 'floatingip',
+      link: 'floating-ip'
+    }, {
+      title: __.snapshot,
+      key: 'snapshots',
+      link: 'snapshot'
+    }];
+
+    this.setState({
+      info: info
+    });
+  }
+
+  renderContent(info, overview) {
+    var halfLength = Math.floor(info.length / 2);
+
+    var leftSide = [],
+      rightSide = [];
+
+    function renderItem(item, index) {
+      return (
+        <div className="item" key={index}>
+          <a data-type="router" href={item.link ? '/project/' + item.link : null}>
+            <div>{item.title}</div>
+            <div>{overview[item.key] ? overview[item.key].used : 0}</div>
+          </a>
+        </div>
+      );
+    }
+
+    for(let i = 0; i < halfLength; i++) {
+      leftSide.push(renderItem(info[i], i));
+      rightSide.push(renderItem(info[i + halfLength], i + halfLength));
+    }
+
+    if (info.length % 2 > 0) {
+      leftSide.push(renderItem(info[halfLength], halfLength));
+    }
+
+    return (
+      <div className="content">
+        <div>{leftSide}</div>
+        <div>{rightSide}</div>
+      </div>
+    );
+  }
+
+  render() {
+    var info = this.state.info,
+      overview = this.props.overview;
+
+    return (
+      <div className="resource-info">
+        <div className="title">{__.resource + __.information}</div>
+        {this.renderContent(info, overview)}
+      </div>
+    );
+  }
+
+}
+
+module.exports = ResourceInfo;
