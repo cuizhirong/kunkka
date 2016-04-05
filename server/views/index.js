31d0b5d9a98e1ab5fe678a504a052dafc0cd233a
Lee Yao <yaoli@unitedstack.com>
2016-04-05 14:48:30 +0800

    fix dashboard ref bug
    Change-Id: If3bfb28881638664cffb158b708ba9e0d5763b08
    Type: BF
    Jira: HALO-377

--- server/views/index.js
+++ server/views/index.js
@@ -11,7 +11,7 @@ var React = require('react');
 var ReactDOMServer = require('react-dom/server');
 
 var loginModel = require('client/login/model.jsx');
-var dashboardModel = require('client/dashboard/model.jsx');
+var dashboardModel = require('client/applications/dashboard/model.jsx');
 var adminModel = require('client/admin/model.jsx');
 
 var config = require('config');
