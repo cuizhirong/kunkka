1d6c1f52947aec7b482bc35a31af2f99a821036c
majiajun <majiajun@unitedstack.com>
2016-04-13 11:19:10 +0800

    filter .DS_Store file
    Change-Id: I0a0f718362255e1eff3fe42af41c4a7c6e21dd90
    Type: BF
    Jira: HALO-387

--- server/views/index.js
+++ server/views/index.js
@@ -7,23 +7,29 @@ require('../helpers/less_register');
 
 const path = require('path');
 const fs = require('fs');
-const frontendApps = fs.readdirSync('client/applications');
-const backendApps = fs.readdirSync('server/api').filter((f) => {
-  return fs.statSync('server/api/' + f).isDirectory();
+const frontendApps = fs.readdirSync('client/applications').filter(file => {
+  return file !== '.DS_Store';
+});
+const backendApps = fs.readdirSync('server/api').filter(file => {
+  return fs.statSync('server/api/' + file).isDirectory();
 });
 const viewsPath = {};
 
 backendApps.forEach((a) => {
   let viewPath = path.join('server/api', a, 'views');
-  fs.readdirSync(viewPath).forEach((p) => {
-    viewsPath[p] = path.join(viewPath, p);
-  });
+  fs.readdirSync(viewPath)
+    .filter(file => {
+      return fs.statSync(path.join(viewPath, file)).isDirectory();
+    })
+    .forEach(p => {
+      viewsPath[p] = path.join(viewPath, p);
+    });
 });
 
 module.exports = (app) => {
   app.set('views', []);
   app.set('applications', frontendApps);
-  frontendApps.forEach((a) => {
+  frontendApps.forEach(a => {
     require(viewsPath[a])(app);
   });
 };
