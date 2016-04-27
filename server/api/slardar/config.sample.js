869fed867af8432fd7eb4d65eb0c9284d3e1bc5b
majiajun <majiajun@unitedstack.com>
2016-04-27 17:34:51 +0800

    refine config
    Change-Id: I7316284fbf6a6c308618c5f42dd44ffde3ebd550
    Type: NF
    Jira: HALO-431

--- server/api/slardar/config.sample.js
+++ server/api/slardar/config.sample.js
@@ -5,12 +5,26 @@ module.exports = {
     'async': '^1.5.0'
   },
   'config': {
+    'region': [{
+      'name': {
+        'en': 'RegionOne',
+        'zh-CN': '一区'
+      },
+      'id': 'RegionOne'
+    }, {
+      'name': {
+        'en': 'RegionTwo',
+        'zh-CN': '二区'
+      },
+      'id': 'RegionOne'
+    }],
     'domain': 'default',
     'cookie': {
       'maxAge': 1000 * 60 * 60 * 24 * 7
     },
+    'port': 5678,
     'extension': {
-      'type': 'example_qiniu'
+      'type': ''
     }
   }
 };
