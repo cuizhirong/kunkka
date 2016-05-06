6106210b356ec6710133a557a17b612c3f3ec0e1
majiajun <majiajun@unitedstack.com>
2016-05-06 22:29:52 +0800

    change remote config to only keystone
    Change-Id: Iea4f84ac870dd8baf90a3d9969abdffe4c88a2c2
    Type: NF
    Jira: HALO-483

--- server/drivers/huskar/config.sample.js
+++ server/drivers/huskar/config.sample.js
@@ -5,20 +5,6 @@ module.exports = {
     'superagent': '^1.4.0'
   },
   'config': {
-    'remote': {
-      'keystone': 'http://42.62.93.98:5000',
-      'nova': {
-        'RegionOne': 'http://42.62.93.98:8774'
-      },
-      'cinder': {
-        'RegionOne': 'http://42.62.93.98:8776'
-      },
-      'neutron': {
-        'RegionOne': 'http://42.62.93.98:9696'
-      },
-      'glance': {
-        'RegionOne': 'http://42.62.93.98:9292'
-      }
-    }
+    'keystone': 'http://42.62.93.98:5000'
   }
 };
