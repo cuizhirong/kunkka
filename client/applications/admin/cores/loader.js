a74b4f8b938cc8c08900b72de921e75106cbc76f
Lee Yao <yaoli@unitedstack.com>
2016-05-31 17:29:55 +0800

    update server-end rendering mechanism
    Change-Id: Iddcda72bdb91a6f216e755f991a412610b9b087d
    Type: NF
    Jira: HALO-488

--- client/applications/admin/cores/loader.js
+++ client/applications/admin/cores/loader.js
@@ -2,14 +2,15 @@
  * @func load modules dynamically
  */
 var configs = require('../config.json');
-
 var modules = {};
 
-configs.modules.forEach((m) => {
-  m.items.forEach((n) => {
-    modules[n] = require('../modules/' + n + '/model');
+if (!HALO.configs.renderer) { // Do not load modules in server-end
+  configs.modules.forEach((m) => {
+    m.items.forEach((n) => {
+      modules[n] = require('../modules/' + n + '/model');
+    });
   });
-});
+}
 
 module.exports = {
   configs: configs,
