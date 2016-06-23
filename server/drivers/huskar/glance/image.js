53a0de322337f44c81ca90876b25f7514e147b54
majiajun <majiajun@unitedstack.com>
2016-06-23 09:54:00 +0800

    update image api
    Change-Id: Idb9458f12b4226879646c3fd7f6eb13ff8581684
    Type: NF
    Jira: HALO-729

--- server/drivers/huskar/glance/image.js
+++ server/drivers/huskar/glance/image.js
@@ -2,6 +2,7 @@
 
 var Base = require('../base.js');
 var driver = new Base();
+var request = require('superagent');
 
 driver.listImages = function (token, remote, callback, query) {
   return driver.getMethod(
@@ -18,6 +19,14 @@ driver.showImageDetails = function (imageId, token, remote, callback, query) {
     callback,
     query
   );
+};
+driver.updateImage = function (imageId, payload, token, remote, callback) {
+  return request
+    .patch(remote + '/v2/images/' + imageId)
+    .set('X-Auth-Token', token)
+    .set('Content-Type', 'application/openstack-images-v2.1-json-patch')
+    .send(JSON.stringify(payload))
+    .end(callback);
 };
 
 module.exports = driver;
