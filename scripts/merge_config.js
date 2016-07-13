78ece9ee09d7903fcb7c6e202e8d2117e21f9ba7
jiangqi <jiangqi@unitedstack.com>
2016-07-13 13:13:24 +0800

    merge the configuration when run npm run pull
    Change-Id: I0599f8d3c8803f220572a6c82f5bc5f1b87e477c
    Type: NF
    Jira: HALO-828

--- scripts/merge_config.js
+++ scripts/merge_config.js
@@ -7,61 +7,24 @@ const driverPath = path.join(__dirname, '../server/drivers');
 const baseConfig = require('../configs/server.sample.js');
 const basePackage = require('../package.sample.json');
 
-const configList = [];
-const addressList = [];
-
-[apiPath, driverPath].forEach( p => {
-  fs.readdirSync(p)
-    .filter( m => fs.statSync(path.join(p, m)).isDirectory())
-    .forEach( f => {
-      try {
-        configList.push(require(path.join(p, f, 'config.sample.js')));
-        addressList.push(path.join(p, f, 'config.sample.js'));
-      } catch (err) {
-        if (err.code !== 'MODULE_NOT_FOUND') {
-          console.log(err);
-        }
-      }
-    });
-});
-
-/* remove other useless characters which don`t cover seperator of version string. */
-const removeOtherChar = function (str, cut) {
-  if (str.constructor === Array) {
-    str.forEach( (s, i) => {
-      str[i] = removeOtherChar(s);
-    });
-  } else {
-    let arr = str.split('');
-    let i = 0;
-    str = '';
-    while(arr[i]) {
-      if (!isNaN(Number(arr[i])) || arr[i] === cut) {
-        str += arr[i];
-      }
-      i++;
-    }
-  }
-  return str;
-};
 
 /* get the newset (or oldest) version. */
-const compareVersion = function (a, b, cut, isNew) {
+const compareVersion = function(a, b, cut, isNew) {
   cut = (cut === undefined) ? '.' : cut;
   isNew = (isNew === undefined) ? true : isNew;
   let arr1 = a.substr('1').split(cut);
   let arr2 = b.substr('1').split(cut);
   let flag; // true: a > b
-  arr1.some( (e, i) => {
+  arr1.some((e, i) => {
     return (arr1[i] !== arr2[i]) && (flag = (arr1[i] > arr2[i]) ? true : false);
   });
   return (isNew === flag) ? a : b;
 };
 
 /* travel every attribute of json. */
-const travel = function (newConfig, oldConfig, isVersion) {
+const travel = function(newConfig, oldConfig, isVersion) {
   isVersion = (isVersion === undefined) ? false : isVersion;
-  for ( let k in newConfig ) {
+  for (let k in newConfig) {
     if (newConfig[k].constructor === Object) {
       if (oldConfig[k] === undefined) {
         oldConfig[k] = {};
@@ -81,22 +44,72 @@ const travel = function (newConfig, oldConfig, isVersion) {
   }
 };
 
+let argv = process.argv.slice(2);
+if (argv.length < 1) {
+  let existConfig;
+
+  try {
+    existConfig = require('../configs/server.json');
+  } catch (e) {
+    existConfig = {};
+  }
+  travel(existConfig, baseConfig);
+}
+
+const configList = [];
+const addressList = [];
+
+[apiPath, driverPath].forEach(p => {
+  fs.readdirSync(p)
+    .filter(m => fs.statSync(path.join(p, m)).isDirectory())
+    .forEach(f => {
+      try {
+        configList.push(require(path.join(p, f, 'config.sample.js')));
+        addressList.push(path.join(p, f, 'config.sample.js'));
+      } catch (err) {
+        if (err.code !== 'MODULE_NOT_FOUND') {
+          console.log(err);
+        }
+      }
+    });
+});
+
+/* remove other useless characters which don`t cover seperator of version string. */
+const removeOtherChar = function(str, cut) {
+  if (str.constructor === Array) {
+    str.forEach((s, i) => {
+      str[i] = removeOtherChar(s);
+    });
+  } else {
+    let arr = str.split('');
+    let i = 0;
+    str = '';
+    while (arr[i]) {
+      if (!isNaN(Number(arr[i])) || arr[i] === cut) {
+        str += arr[i];
+      }
+      i++;
+    }
+  }
+  return str;
+};
+
 const smap = new Map();
 
-const connect = function(pre, k){
+const connect = function(pre, k) {
   return pre.concat('.', k);
 };
 
-const checkRepeatConfig = function(config, CRpath, pre){
-  for(let k in config){
-    if(config[k].constructor === Object){
-      if(pre === undefined){
+const checkRepeatConfig = function(config, CRpath, pre) {
+  for (let k in config) {
+    if (config[k].constructor === Object) {
+      if (pre === undefined) {
         checkRepeatConfig(config[k], CRpath, k);
       } else {
         checkRepeatConfig(config[k], CRpath, connect(pre, k));
       }
     }
-    if(config[k].constructor !== Object) {
+    if (config[k].constructor !== Object) {
       if (pre === undefined) {
         if (!smap.has(k)) {
           let lpath = [];
@@ -118,31 +131,33 @@ const checkRepeatConfig = function(config, CRpath, pre){
   }
 };
 
-for(let i = 0; i < configList.length; i++){
+for (let i = 0; i < configList.length; i++) {
   checkRepeatConfig(configList[i].config, addressList[i]);
 }
 
 const baseConfigPath = path.join(__dirname, '../configs/server.sample.js');
 checkRepeatConfig(baseConfig, baseConfigPath);
 
-for (let item of smap.entries()) {
-  if(item[1].length !== 1){
-    console.log('warning! ' + item[0] + ' is duplicate in those files:');
-    for(let dir of item[1]){
-      console.log(dir);
+if (argv.length > 0) {
+  for (let item of smap.entries()) {
+    if (item[1].length !== 1) {
+      console.log('warning! ' + item[0] + ' is duplicate in those files:');
+      for (let dir of item[1]) {
+        console.log(dir);
+      }
     }
   }
 }
 
 
-configList.forEach( con => {
+configList.forEach(con => {
   travel(con.config, baseConfig);
   travel(con.dependencies, basePackage.dependencies, true);
 });
 
 /* generate mixed json file. */
 
-const generateFile = function (arr) {
+const generateFile = function(arr) {
   fs.writeFile(arr[0]._path, JSON.stringify(arr[0]._str, null, 2), err => {
     if (err) {
       console.log(err);
@@ -158,9 +173,12 @@ const generateFile = function (arr) {
   });
 };
 
-const _arr = [
-  {_path: path.join(__dirname, '../configs/server.json'), _str: baseConfig},
-  {_path: path.join(__dirname, '../package.json'), _str: basePackage}
-];
+const _arr = [{
+  _path: path.join(__dirname, '../configs/server.json'),
+  _str: baseConfig
+}, {
+  _path: path.join(__dirname, '../package.json'),
+  _str: basePackage
+}];
 
 generateFile(_arr);
