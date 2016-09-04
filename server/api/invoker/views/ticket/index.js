960b90969a46dd1eacd788465085b94dd346ec70
majiajun <majiajun@unitedstack.com>
2016-09-04 15:13:17 +0800

    refine view module
    Change-Id: I2c254ec13c4a9f4f1c71f8e38bf1be1dfcdf0fd3
    Type: OT
    Jira: HALO-984

--- server/api/invoker/views/ticket/index.js
+++ server/api/invoker/views/ticket/index.js
@@ -11,13 +11,6 @@ function main(app, clientApps, currentView, viewPlugins) {
   views.push(__dirname);
   const view = new View(app, clientApps, currentView, viewPlugins);
   view.init();
-  view.renderChecher = function(setting, req, res, next) {
-    if (req.session && req.session.user && setting.enable_ticket) {
-      this.renderTemplate(setting, req, res, next);
-    } else {
-      res.redirect('/');
-    }
-  };
 }
 
 function haloProcessor(user, HALO) {
