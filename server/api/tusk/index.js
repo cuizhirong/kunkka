d29a6962422b83c497259d3ba0d45f559c7d40be
majiajun <majiajun@unitedstack.com>
2016-09-19 17:06:56 +0800

    add admin check for api
    Change-Id: Ia735da733d23150a3bdfbabf1225d1e2a7596dda
    Type: BF
    Jira: HALO-981

--- server/api/tusk/index.js
+++ server/api/tusk/index.js
@@ -2,7 +2,7 @@
 
 module.exports = function(app) {
   app.use('/api/setting', function (req, res, next) {
-    if (req.session.user) {
+    if (req.session.user && req.session.user.isAdmin) {
       next();
     } else {
       return res.status(401).json({error: req.i18n.__('api.auth.unauthorized')});
