6731c438f8cfe4d2426d50a72115be5e08fa356c
PengJiyuan <731401082@qq.com>
2016-10-25 15:43:26 +0800

    single login
    Change-Id: Ifd64efc65abd0735c7f71db915e2f4a07a6b0313
    Type: NF
    Jira: HALO-1154

--- client/applications/login/index.jsx
+++ client/applications/login/index.jsx
@@ -3,15 +3,14 @@ require('./style/index.less');
 var ReactDOM = require('react-dom');
 var React = require('react');
 var Model = require('./model');
-var __ = require('locale/client/auth.lang.json');
+var __ = require('locale/client/login.lang.json');
 
 var loginModel = React.createFactory(Model);
 
 ReactDOM.render(
   loginModel({
     HALO: HALO,
-    __: __,
-    path: window.location.pathname
+    __: __
   }),
   document.getElementsByClassName('input-wrapper')[0]
 );
--- client/applications/login/model.jsx
+++ client/applications/login/model.jsx
@@ -1,9 +1,5 @@
 let React = require('react');
 let request = require('./request.js');
-let Input = require('./components/input/index.jsx');
-let Phone = require('./components/phone/index.jsx');
-let notification = require('client/uskin/index').Notification;
-let getErrorMessage = require('./utils/error_message.js');
 
 class Model extends React.Component {
 
@@ -11,55 +7,15 @@ class Model extends React.Component {
     super(props);
 
     this.state = {
-      isLogin: props.HALO.settings.enable_register ? !(props.path === '/signup') : true,
       loginError: false,
       usernameEmptyError: false,
       passwordEmptyError: false,
       isSubmitting: false,
-      hasRead: false,
-      email_tip: props.__.email_tip,
-      account_tip: props.__.account_tip,
-      timer: null,
-      settings: props.HALO.settings,
       domains: (props.HALO.settings.domains.indexOf('Default') > -1 ? 'Default' : props.HALO.settings.domains[0])
     };
 
-    ['onSubmit', 'onClick', 'onChange'].forEach(item => {
+    ['onSubmit', 'onChange'].forEach(item => {
       this[item] = this[item].bind(this);
-    });
-  }
-
-  componentDidMount() {
-    let wrapper = document.getElementById('container').parentNode;
-    let content = wrapper.parentNode;
-    content.style.height = !this.state.isLogin ? '530px' : '434px';
-    wrapper.style.marginTop = !this.state.isLogin ? '32px' : '58px';
-  }
-
-  errorReport(err) {
-    notification.addNotice({
-      showIcon: true,
-      content: err || '',
-      isAutoHide: true,
-      type: 'danger',
-      width: 300,
-      id: Date.now()
-    });
-  }
-
-  onClick(e) {
-    let wrapper = document.getElementById('container').parentNode;
-    let content = wrapper.parentNode;
-    content.style.height = this.state.isLogin ? '530px' : '434px';
-    wrapper.style.marginTop = this.state.isLogin ? '32px' : '58px';
-    this.setState({
-      isLogin: !this.state.isLogin
-    }, () => {
-      if(!this.state.isLogin) {
-        window.history.pushState({}, null, '/signup');
-      } else {
-        window.history.pushState({}, null, '/');
-      }
     });
   }
 
@@ -69,244 +25,60 @@ class Model extends React.Component {
       that = this,
       state = that.state;
 
-    if (this.state.isSubmitting) {
+    if (state.isSubmitting) {
       return;
     }
 
-    if(this.state.isLogin) {
-      let username = refs.username.value,
-        password = refs.password.value,
-        data = {
-          username: username,
-          password: password
-        };
-
-      if(state.settings.enable_domain) {
-        data.domain = refs.domains.value;
-      }
-
-      that.setState({
-        loginError: false,
-        usernameEmptyError: false,
-        passwordEmptyError: false
-      });
-
-      let isEmpty = !username || !password;
-      if (isEmpty) {
-        if (!username) {
-          that.setState({
-            usernameEmptyError: true
-          });
-        }
-        if (!password) {
-          that.setState({
-            passwordEmptyError: true
-          });
-        }
-        return;
-      }
-
-      that.setState({
-        isSubmitting: true
-      });
-
-      request.login(data).then(function(res) {
-        window.location = window.location.pathname;
-      }, function(err) {
-        that.setState({
-          loginError: true,
-          isSubmitting: false
-        });
-      });
-    } else {
-      if(!this.state.canSubmit) {
-        return;
-      }
-      let data = {
-        name: refs.name.state.value,
-        email: refs.email.state.value,
-        phone: refs.phone.state.value,
-        code: refs.code.state.value,
-        password: refs.password.state.value
+    let username = refs.username.value,
+      password = refs.password.value,
+      data = {
+        username: username,
+        password: password
       };
-      that.setState({
-        isSubmitting: true
-      });
-      request.registerAccount(data).then((res) => {
-        window.location = '/auth/register/success?email=' + data.email;
-      }).catch((err) => {
-        let errorMsg = getErrorMessage(err, true);
-        that.setState({
-          isSubmitting: false
-        });
-        if(errorMsg.type === 'SystemError') {
-          this.errorReport(errorMsg.message);
-        } else {
-          errorMsg.location.forEach((item) => {
-            refs[item].setState({
-              error: true,
-              loading: false,
-              pass: false,
-              showTip: true,
-              tipContent: errorMsg.message
-            });
-          });
-        }
-      });
 
+    if(HALO.settings.enable_domain) {
+      data.domain = refs.domains.value;
     }
 
+    that.setState({
+      loginError: false,
+      usernameEmptyError: false,
+      passwordEmptyError: false
+    });
+
+    let isEmpty = !username || !password;
+    if (isEmpty) {
+      if (!username) {
+        that.setState({
+          usernameEmptyError: true
+        });
+      }
+      if (!password) {
+        that.setState({
+          passwordEmptyError: true
+        });
+      }
+      return;
+    }
+
+    that.setState({
+      isSubmitting: true
+    });
+
+    request.login(data).then(function(res) {
+      window.location = window.location.pathname;
+    }, function(err) {
+      that.setState({
+        loginError: true,
+        isSubmitting: false
+      });
+    });
   }
 
   onChange(type, value, e) {
-    let refs = this.refs,
-      __ = this.props.__,
-      that = this,
-      canSubmit = false,
-      canSub = function() {
-        canSubmit = refs.email.state.pass && refs.name.state.pass
-          && refs.password.state.pass && refs.confirm_password.state.pass
-          && refs.phone.state.pass && (that.state.settings.eula_content ? that.state.hasRead : true);
-        that.setState({
-          canSubmit: canSubmit
-        });
-      },
-      error = function(message) {
-        refs[type].setState({
-          error: true,
-          loading: false,
-          pass: false,
-          showTip: true,
-          tipContent: message ? message : __[type + '_tip']
-        }, canSub);
-      },
-      loading = function() {
-        refs[type].setState({
-          error: false,
-          loading: true,
-          pass: false,
-          showTip: false
-        }, canSub);
-      },
-      pass = function() {
-        refs[type].setState({
-          error: false,
-          pass: true,
-          loading: false
-        }, canSub);
-      };
+    let refs = this.refs;
 
     switch(type) {
-      case 'email':
-        let regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+\.([a-zA-Z0-9])+/;
-        if(!regEmail.test(value)) {
-          if(this.state.timer) {
-            clearTimeout(this.state.timer);
-          }
-          error();
-        } else {
-          loading();
-          if(this.state.timer) {
-            clearTimeout(this.state.timer);
-          }
-          this.state.timer = setTimeout(() => {
-            request.verifyEmail(value).then((res) => {
-              pass();
-            }).catch((err) => {
-              error(getErrorMessage(err));
-            });
-          }, 500);
-        }
-        break;
-      case 'name':
-        if(value.length > 20 || value.length < 1) {
-          if(this.state.timer) {
-            clearTimeout(this.state.timer);
-          }
-          error();
-        } else {
-          loading();
-          if(this.state.timer) {
-            clearTimeout(this.state.timer);
-          }
-          this.state.timer = setTimeout(() => {
-            request.verifyUsername(value).then((res) => {
-              pass();
-            }).catch((err) => {
-              error(getErrorMessage(err));
-            });
-          }, 500);
-        }
-        break;
-      case 'password':
-        let checkPsw = function(password) {
-          return (password.length < 8 || password.length > 20 || !/^[a-zA-Z0-9]/.test(password) || !/[a-z]+/.test(password) || !/[A-Z]+/.test(password) || !/[0-9]+/.test(password));
-        };
-        if(checkPsw(value)) {
-          refs.password.setState({
-            error: true,
-            pass: false
-          }, canSub);
-        } else {
-          refs.password.setState({
-            error: false,
-            pass: true
-          }, canSub);
-        }
-        if(refs.confirm_password.state.value !== '') {
-          if(value !== refs.confirm_password.state.value) {
-            refs.confirm_password.setState({
-              error: true,
-              pass: false
-            }, canSub);
-          } else {
-            refs.confirm_password.setState({
-              error: false,
-              pass: true
-            }, canSub);
-          }
-        }
-        break;
-      case 'confirm_password':
-        if(value !== refs.password.state.value) {
-          refs.confirm_password.setState({
-            error: true,
-            pass: false
-          }, canSub);
-        } else {
-          refs.confirm_password.setState({
-            error: false,
-            pass: true
-          }, canSub);
-        }
-        break;
-      case 'phone':
-        let regPhone = /^1[3,4,5,6,7,8]\d{9}$/;
-        if(!regPhone.test(value)) {
-          refs.phone.setState({
-            error: true,
-            pass: false
-          }, canSub);
-        } else {
-          refs.phone.setState({
-            error: false,
-            pass: true,
-            tipContent: null
-          }, canSub);
-        }
-        break;
-      case 'code':
-        refs.code.setState({
-          error: false,
-          showTip: false,
-          tipContent: null
-        });
-        break;
-      case 'readme':
-        this.setState({
-          hasRead: !this.state.hasRead
-        }, canSub);
-        break;
       case 'domains':
         this.setState({
           domains: refs.domains.value
@@ -320,75 +92,46 @@ class Model extends React.Component {
   render() {
     let props = this.props,
       state = this.state,
-      __ = props.__,
-      domains = state.settings.domains;
+      HALO = props.HALO,
+      __ = props.__;
 
-    if(state.isLogin) {
-      return (
-        <div id="container">
-          <form method="POST" onSubmit={this.onSubmit}>
-            <input type="text" ref="username" name="username" className={state.usernameEmptyError ? 'error' : ''} placeholder={__.account_placeholder} autoFocus="autofocus" autoComplete="off" />
-            <input type="password" ref="password" name="password" className={state.passwordEmptyError ? 'error' : ''} placeholder={__.password_placeholder} autoComplete="off" />
-            {
-              state.settings.enable_domain ? <select ref="domains" value={state.domains} onChange={this.onChange.bind(this, 'domains', '')}>
-                {
-                  domains.map((domain) => {
-                    return <option key={domain} value={domain}>{domain}</option>;
-                  })
-                }
-              </select> : null
-            }
-             <div className="find-Password">
-            {
-              this.state.settings.enable_register ? <a href="/auth/password"> {__.forgotPass}</a> : null
-            }
-            </div>
-            <div className="tip-wrapper">
-              <div className={'input-error' + (state.loginError ? '' : ' hide')}>
-                <i className="glyphicon icon-status-warning"></i><span>{__.error_tip}</span>
-              </div>
-            </div>
-            <input type="submit" className={state.isSubmitting ? 'disabled' : ''} value={__.submit} />
-          </form>
+    return (
+      <div id="container">
+        <form method="POST" onSubmit={this.onSubmit}>
+          <input type="text" ref="username" name="username" className={state.usernameEmptyError ? 'error' : ''} placeholder={__.account_placeholder} autoFocus="autofocus" autoComplete="off" />
+          <input type="password" ref="password" name="password" className={state.passwordEmptyError ? 'error' : ''} placeholder={__.password_placeholder} autoComplete="off" />
           {
-            state.settings.enable_register ? <div className="link">
-              {__.hasAccount}
-              <span>|</span>
-              {__.isNoAccount}<a onClick={this.onClick}>{__.signup}</a>
-            </div>
-            : null
-          }
-        </div>
-      );
-    } else {
-      return (
-        <div id="container">
-          <form method="POST" onSubmit={this.onSubmit}>
-            <Input input_type="text" ref="email" name="email" __={__} tip={true} placeholder={__.email_placeholder} onChange={this.onChange} />
-            <Input input_type="text" ref="name" name="name" __={__} tip={true} placeholder={__.name_placeholder} onChange={this.onChange} />
-            <Input input_type="password" ref="password" name="password" __={__} tip={true} placeholder={__.password_placeholder} onChange={this.onChange} />
-            <Input input_type="password" ref="confirm_password" name="confirm_password" onChange={this.onChange} placeholder={__.confirm_password_placeholder} />
-            <Phone ref="phone" name="phone" __={__} onChange={this.onChange} />
-            <Input input_type="text" ref="code" name="code" __={__} tip={true} placeholder={__.code_placeholder} onChange={this.onChange} />
-            <div key="checkbox" className="checkbox">
+
+            HALO.settings.enable_domain ? <select ref="domains" value={state.domains} onChange={this.onChange.bind(this, 'domains', '')}>
               {
-                state.settings.eula_content ? <div>
-                  <input name="readme" value="null" type="checkbox" checked={this.state.hasRead} onChange={this.onChange.bind(this, 'readme', '')}/>
-                  <span>{__.readAgree}<a href={state.settings.eula_content}>{__.eula}</a></span>
-                </div>
-                : null
+                HALO.settings.domains.map((domain) => {
+                  return <option key={domain} value={domain}>{domain}</option>;
+                })
               }
+            </select> : null
+          }
+          {
+            HALO.settings.enable_register ? <div className="find-Password">
+                <a href="/auth/password"> {__.forgotPass}</a>
+              </div> : null
+          }
+          <div className="tip-wrapper">
+            <div className={'input-error' + (state.loginError ? '' : ' hide')}>
+              <i className="glyphicon icon-status-warning"></i><span>{__.error_tip}</span>
             </div>
-            <input type="submit" className={(state.isSubmitting || !state.canSubmit) ? 'disabled' : ''} value={__.signup} />
-          </form>
-          <div className="link">
-            {__.noAccount}
-            <span>|</span>
-            {__.isHasAccount}<a onClick={this.onClick}>{__.submit}</a>
           </div>
-        </div>
-      );
-    }
+          <input type="submit" className={state.isSubmitting ? 'disabled' : ''} value={__.login} />
+        </form>
+        {
+          HALO.settings.enable_register ? <div className="link">
+            {__.hasAccount}
+            <span>|</span>
+            {__.isNoAccount}<a href="/register">{__.signup}</a>
+          </div>
+          : <div className="link"></div>
+        }
+      </div>
+    );
   }
 }
 
