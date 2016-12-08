247c15b4aea7f8a524f2db7c898cc57bad7eb687
majiajun <majiajun@unitedstack.com>
2016-12-08 17:26:04 +0800

    add description in setting
    Change-Id: I95e57fc060b433293112c9d96c661e610b42607b
    Type: NF
    Jira: HALO-1365

--- server/api/tusk/config.sample.js
+++ server/api/tusk/config.sample.js
@@ -13,28 +13,32 @@ module.exports = {
       app: 'global',
       name: 'logo_url',
       value: '/static/assets/nav_logo.png',
-      type: 'string'
+      type: 'string',
+      description: 'logo 图片地址'
     }, {
       app: 'global',
       name: 'favicon',
       value: '/static/assets/favicon.ico',
-      type: 'string'
+      type: 'string',
+      description: '浏览器 tab 小图标'
     }, {
       app: 'global',
       name: 'title',
       value: 'UnitedStack 有云',
-      type: 'string'
+      type: 'string',
+      description: '浏览器 tab 标题'
     }, {
       app: 'global',
       name: 'default_image_url',
       value: '',
       type: 'string',
-      description: '图片大小为40*40'
+      description: '默认 image 图片， 图片大小为40*40'
     }, {
       app: 'global',
       name: 'enable_charge',
       value: 'false',
-      type: 'boolean'
+      type: 'boolean',
+      description: '开启计费功能'
     }
   ]
 };
