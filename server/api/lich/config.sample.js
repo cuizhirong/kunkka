6fb883a3c9d779bb272b6a306b17ed49271fb79e
majiajun <majiajun@unitedstack.com>
2016-12-08 17:32:30 +0800

    add description in setting
    Change-Id: I523b94583e7d0859f28c95cbb84c9ccac2bd7cf1
    Type: NF
    Jira: HALO-1365

--- server/api/lich/config.sample.js
+++ server/api/lich/config.sample.js
@@ -12,31 +12,37 @@ module.exports = {
     app: 'global',
     name: 'enable_approval',
     value: 'false',
-    type: 'boolean'
+    type: 'boolean',
+    description: '开启审批功能'
   }, {
     app: 'approval',
     name: 'view.css',
     value: '',
-    type: 'text'
+    type: 'text',
+    description: '审批面板-样式定制'
   }, {
     app: 'approval',
     name: 'appliable_volume_types',
     value: '["ssd", "capacity"]',
-    type: 'string'
+    type: 'string',
+    description: '审批面板-允许申请的云硬盘类型'
   }, {
     app: 'approval',
     name: 'enable_apply_instance_credential',
     value: 'true',
-    type: 'boolean'
+    type: 'boolean',
+    description: '审批面板-允许用户设置虚拟机秘密'
   }, {
     app: 'approval',
     name: 'enable_apply_instance_name',
     value: 'true',
-    type: 'boolean'
+    type: 'boolean',
+    description: '审批面板-允许用户设置虚拟机名称'
   }, {
     app: 'approval',
     name: 'server_name_prefix',
     value: 'op',
-    type: 'string'
+    type: 'string',
+    description: '审批面板-虚拟机名称前缀'
   }]
 };
