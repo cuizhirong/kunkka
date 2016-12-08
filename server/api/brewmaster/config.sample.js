5deeb43da585b516befb277d60fa998e05921bf7
majiajun <majiajun@unitedstack.com>
2016-12-08 17:27:00 +0800

    add description to setting
    Change-Id: I5772cc7a5fb4e8f8712a9b7c22900e6c161fadb1
    Type: NF
    Jira: HALO-1365

--- server/api/brewmaster/config.sample.js
+++ server/api/brewmaster/config.sample.js
@@ -14,54 +14,64 @@ module.exports = {
       app: 'login',
       name: 'logo_url',
       value: '/static/assets/login/logo@2x.png',
-      type: 'string'
+      type: 'string',
+      description: '登录界面- logo 地址'
     }, {
       app: 'login',
       name: 'company',
       value: '©2016 UnitedStack Inc. All Rights Reserved. 京ICP备13015821号',
-      type: 'string'
+      type: 'string',
+      description: '登录界面-公司 copyright'
     }, {
       app: 'login',
       name: 'corporation_name',
       value: 'UnitedStack 有云',
-      type: 'string'
+      type: 'string',
+      description: '登录界面-公司名称'
     }, {
       app: 'login',
       name: 'enable_domain',
       value: 'false',
-      type: 'boolean'
+      type: 'boolean',
+      description: '登录界面-支持多 domain 功能'
     }, {
       app: 'global',
       name: 'enable_register',
       value: 'false',
-      type: 'boolean'
+      type: 'boolean',
+      description: '开启注册功能'
     },
     {
       app: 'register',
       name: 'logo_url',
       value: '/static/assets/register/logo@2x.png',
-      type: 'string'
+      type: 'string',
+      description: '注册页面- logo 地址'
     }, {
       app: 'register',
       name: 'company',
       value: '©2016 UnitedStack Inc. All Rights Reserved. 京ICP备13015821号',
-      type: 'string'
+      type: 'string',
+      description: '登录界面-公司 copyright'
     }, {
       app: 'register',
       name: 'corporation_name',
       value: 'UnitedStack 有云',
-      type: 'string'
+      type: 'string',
+      description: '登录界面-公司名称'
     },
     {
       app: 'register',
       name: 'eula_content',
       value: '',
-      type: 'string'
+      type: 'string',
+      description: '注册页面-用户协议页面地址'
     }, {
       app: 'register',
       name: 'enable_domain',
       value: 'false',
-      type: 'boolean'
+      type: 'boolean',
+      description: '注册页面-支持多 domain 功能'
     }
   ]
 };
