416432860417a258b0118062bf34821bc0156732
wanghuanan <wanghuanan@unitedstack.com>
2016-12-15 10:39:58 +0800

    support define migrate dest by id
    Change-Id: Iab72cb5e7ce7bf340614889beca9cee78b5131aa
    Type: BF
    Jira: HALO-1375

--- client/applications/admin/modules/host/pop/migrate/index.js
+++ client/applications/admin/modules/host/pop/migrate/index.js
@@ -2,6 +2,7 @@ var commonModal = require('client/components/modal_common/index');
 var config = require('./config.json');
 var request = require('../../request');
 var __ = require('locale/client/admin.lang.json');
+var getErrorMessage = require('client/applications/admin/utils/error_message');
 
 function pop(obj, parent, callback) {
   var {row, hostTypes} = obj;
@@ -37,18 +38,60 @@ function pop(obj, parent, callback) {
       });
     },
     onConfirm: function(refs, cb) {
-      var dest = refs.migrate_to.state.value;
-      if (dest === 'auto') {
-        dest = null;
-      }
-      var randomly = refs.migrate_host_tip.state.checked;
+      let randomly = refs.migrate_host_tip.state.checked,
+        checkedField = refs.migrate_to.state.checkedField,
+        dest = '';
+      if(checkedField === 'migrate_to') {//set dest by select
+        dest = refs.migrate_to.state.value;
+        if (dest === 'auto') {
+          dest = null;
+        }
+        request.migrate(currentHost, dest, randomly);
+        cb(true);
+      } else {//set dest by id
+        dest = refs.migrate_to_other.state.value;
 
-      cb(true);
-      request.migrate(currentHost, dest, randomly);
+        request.getHypervisorById(dest).then(host => {
+          request.migrate(currentHost, dest, randomly);
+          cb(true);
+        }).catch((err) => {
+          cb(false, getErrorMessage(err));
+        });
+      }
     },
     onAction: function(field, state, refs) {
-    },
-    onLinkClick: function() {
+      switch(field) {
+        case 'migrate_to':
+          refs.migrate_to_other.setState({
+            checkedField: state.checkedField
+          });
+
+          if(state.checkedField === 'migrate_to') {
+            let selectDisabled = refs.migrate_to.state.data.length === 0;
+            refs.btn.setState({
+              disabled: selectDisabled
+            });
+          }
+          break;
+        case 'migrate_to_other':
+          refs.migrate_to.setState({
+            checkedField: state.checkedField
+          });
+          let host = refs.migrate_to_other.state.value,
+            hostFail = (host === row.id.toString()) || (host === '');
+          refs.migrate_to_other.setState({
+            error: hostFail
+          });
+          break;
+        default:
+          break;
+      }
+
+      let enableBtn = (state.checkedField === 'migrate_to' && !!refs.migrate_to.state.value)
+        || (state.checkedField === 'migrate_to_other' && !refs.migrate_to_other.state.error);
+      refs.btn.setState({
+        disabled: !enableBtn
+      });
     }
   };
 
