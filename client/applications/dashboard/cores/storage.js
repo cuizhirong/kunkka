472873b8873d4098f359d44d19f5d9bdb3dd3394
cuizhirong <cuizhirong@unitedstack.com>
2017-07-05 10:58:42 +0800

    hidden heat
    Change-Id: I86074b280d87e8e55ac1d278d7b2a66bca13b9ed
    Type: NF
    Jira: HALO-1733

--- client/applications/dashboard/cores/storage.js
+++ client/applications/dashboard/cores/storage.js
@@ -29,9 +29,6 @@ var ikepolicy = require('../modules/ike-policy/cache');
 var ipsecpolicy = require('../modules/ipsec-policy/cache');
 var notification = require('../modules/notification/cache');
 var alarm = require('../modules/alarm/cache');
-var heat = require('../modules/heat-list/cache');
-var resourcetype = require('../modules/resource-type/cache');
-var templateversion = require('../modules/template-version/cache');
 
 var map = {
   network: ['subnet'],
@@ -91,6 +88,6 @@ Storage.prototype = {
 
 Object.assign(Storage.prototype, instance, image, port, floatingip, keypair, network, router, securitygroup,
   snapshot, subnet, volume, loadbalancer, pool, listener, healthmonitor, member, vpnservice, ipsec, ikepolicy,
-  ipsecpolicy, notification, alarm, heat, resourcetype, templateversion);
+  ipsecpolicy, notification, alarm);
 
 module.exports = new Storage();
