7653e6e30980f9eec142bb639c4b72a404b758a6
majiajun <majiajun@unitedstack.com>
2017-07-12 13:22:55 +0800

    all get tag based on branch
    Change-Id: I91aae8d5050318b9d5921c7d46da74fa08a80ff1
    Type: NF

--- scripts/get_tag.js
+++ scripts/get_tag.js
@@ -7,6 +7,11 @@ const http = require('http');
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
+const branch = process.argv[2] || 'master';
+const tagNumber = {
+  'stable/liberty': 3
+};
+
 // please create a token.json file first!
 let token;
 try {
@@ -17,10 +22,10 @@ try {
 }
 
 function split (flag, version) {
-  var result = [];
+  let result = [];
   if (flag) {
-    var tail = version.split('-')[1];
-    var _version = version.split('-')[0];
+    let tail = version.split('-')[1];
+    let _version = version.split('-')[0];
     result = _version.split('.');
     tail = tail.split('.');
     result = result.concat(tail);
@@ -37,14 +42,14 @@ function convertToNumber (arr) {
 }
 
 function compare (v1, v2) {
-  var flag1 = v1.indexOf('-') > -1 ? true : false;
-  var flag2 = v2.indexOf('-') > -1 ? true : false;
-  var arr1 = split(flag1, v1);
-  var arr2 = split(flag2, v2);
+  let flag1 = v1.indexOf('-') > -1 ? true : false;
+  let flag2 = v2.indexOf('-') > -1 ? true : false;
+  let arr1 = split(flag1, v1);
+  let arr2 = split(flag2, v2);
   arr1 = convertToNumber(arr1);
   arr2 = convertToNumber(arr2);
-  var len = Math.max(arr1.length, arr2.length);
-  for (var i = 0; i < len; i ++) {
+  let len = Math.max(arr1.length, arr2.length);
+  for (let i = 0; i < len; i ++) {
     if (arr1[i] === undefined) {
       return -1;
     } else if (arr2[i] === undefined) {
@@ -57,6 +62,13 @@ function compare (v1, v2) {
     }
   }
   return 0;
+}
+
+function filterTags (tag, _branch) {
+  let flag = tag.indexOf('-') > -1;
+  let arr = split(flag, tag);
+  arr = convertToNumber(arr);
+  return arr[0] === tagNumber[_branch];
 }
 
 const len = applications.length;
@@ -76,6 +88,10 @@ for (let i = 0; i < len; i++) {
     });
     res.on('end', () => {
       let tags = JSON.parse(data);
+      if (branch !== 'master') {
+        tags = tags.filter(tag => filterTags(tag.name, branch));
+      }
+      console.log(tags);
       tags.sort((a, b) => compare(b.name, a.name));
       applications[i].branch.production = tags[0].name;
       emitter.emit('finish');
@@ -106,6 +122,9 @@ let request = http.request(haloOpt, (res) => {
   });
   res.on('end', () => {
     let tags = JSON.parse(data);
+    if (branch !== 'master') {
+      tags = tags.filter(tag => filterTags(tag.name, branch));
+    }
     tags.sort((a, b) => compare(b.name, a.name));
     let latestTag = tags[0].name;
     config.halo = latestTag;
