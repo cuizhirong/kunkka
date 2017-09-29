6d7998b4ae77379b17bfc8cc19cdc5c98ef91b10
PengJiyuan <pengjiyuan@unitedstack.com>
2017-09-29 16:12:55 +0800

    navbar config items
    Type: NF
    Jira: HALO-1802

--- client/applications/bill/model.jsx
+++ client/applications/bill/model.jsx
@@ -122,7 +122,7 @@ class Model extends React.Component {
     return (
       <div id="wrapper">
         <div id="navbar">
-          <NavBar HALO={HALO} __={__} hideRegion={true} hideProject={true} />
+          <NavBar HALO={HALO} __={__} setting={true} />
         </div>
         <div id="main-wrapper">
           <SideMenu items={menus} application={HALO.application} />
