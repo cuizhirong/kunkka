001335d609546f0e27f3c8e962913c1201b507ce
PengJiyuan <731401082@qq.com>
2017-09-29 19:33:31 +0800

    import fetch from component
    Change-Id: I99d3954c2721374fcc967b966997664ede4ccf94
    Type: NF
    Jira: HALO-1821

--- client/applications/admin/cores/fetch.js
+++ client/applications/admin/cores/fetch.js
@@ -1,30 +1,3 @@
-const request = require('client/libs/ajax');
-const RSVP = require('rsvp');
-const Promise = RSVP.Promise;
-
-function errHandler(err) {
-  if (err.status === 401) {
-    window.location = '/auth/logout';
-  }
-  return new Promise(function(resolve, reject) {
-    reject(err);
-  });
-}
-
-let fetch = {};
-
-['get', 'post', 'put', 'delete', 'patch'].forEach((m) => {
-  fetch[m] = function(options) {
-    let opt = Object.assign({
-      dataType: 'json',
-      contentType: 'application/json',
-      headers: {
-        REGION: HALO.current_region
-      }
-    }, options);
-
-    return request[m](opt).catch(errHandler);
-  };
-});
+const fetch = require('client/libs/fetch');
 
 module.exports = fetch;
