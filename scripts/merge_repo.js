afe9f319941dbf7bb843295a3c72965b12d89bff
PengJiyuan <731401082@qq.com>
2017-10-23 19:56:55 +0800

    add package.json, remove sample. update .gitignore

--- scripts/merge_repo.js
+++ scripts/merge_repo.js
@@ -1,113 +1,113 @@
 const items = [
   {
-    "project": "spectre",
-    "name": "dashboard",
-    "branch": {
-      "dev": "master",
-      "production": "4.0.4"
+    'project': 'spectre',
+    'name': 'dashboard',
+    'branch': {
+      'dev': 'master',
+      'production': '4.0.4'
     },
-    "git": "git@github.com:unitedstack/spectre.git",
-    "directory": "client/applications"
+    'git': 'git@github.com:unitedstack/spectre.git',
+    'directory': 'client/applications'
   },
   {
-    "project": "luna",
-    "name": "login",
-    "branch": {
-      "dev": "master",
-      "production": "4.0.1"
+    'project': 'luna',
+    'name': 'login',
+    'branch': {
+      'dev': 'master',
+      'production': '4.0.1'
     },
-    "git": "git@github.com:unitedstack/luna.git",
-    "directory": "client/applications"
+    'git': 'git@github.com:unitedstack/luna.git',
+    'directory': 'client/applications'
   },
   {
-    "project": "medusa",
-    "name": "admin",
-    "branch": {
-      "dev": "master",
-      "production": "4.0.4"
+    'project': 'medusa',
+    'name': 'admin',
+    'branch': {
+      'dev': 'master',
+      'production': '4.0.4'
     },
-    "git": "git@github.com:unitedstack/medusa.git",
-    "directory": "client/applications"
+    'git': 'git@github.com:unitedstack/medusa.git',
+    'directory': 'client/applications'
   },
   {
-    "project": "lion",
-    "name": "register",
-    "branch": {
-    "dev": "master",
-      "production": "4.0.0"
+    'project': 'lion',
+    'name': 'register',
+    'branch': {
+      'dev': 'master',
+      'production': '4.0.0'
     },
-    "git": "git@github.com:unitedstack/lion.git",
-    "directory": "client/applications"
+    'git': 'git@github.com:unitedstack/lion.git',
+    'directory': 'client/applications'
   },
   {
-    "project": "huskar",
-    "name": "huskar",
-    "branch": {
-      "dev": "master",
-      "production": "4.0.0"
+    'project': 'huskar',
+    'name': 'huskar',
+    'branch': {
+      'dev': 'master',
+      'production': '4.0.0'
     },
-    "git": "git@github.com:unitedstack/huskar.git",
-    "directory": "server/drivers"
+    'git': 'git@github.com:unitedstack/huskar.git',
+    'directory': 'server/drivers'
   },
-{
-    "project": "slardar",
-    "name": "slardar",
-    "branch": {
-      "dev": "master",
-      "production": "4.0.2"
+  {
+    'project': 'slardar',
+    'name': 'slardar',
+    'branch': {
+      'dev': 'master',
+      'production': '4.0.2'
     },
-    "git": "git@github.com:unitedstack/slardar.git",
-    "directory": "server/api"
+    'git': 'git@github.com:unitedstack/slardar.git',
+    'directory': 'server/api'
   },
   {
-    "project": "tusk",
-    "name": "tusk",
-    "branch": {
-      "dev": "master",
-      "production": "4.0.1"
+    'project': 'tusk',
+    'name': 'tusk',
+    'branch': {
+      'dev': 'master',
+      'production': '4.0.1'
     },
-    "git": "git@github.com:unitedstack/tusk.git",
-    "directory": "server/api"
+    'git': 'git@github.com:unitedstack/tusk.git',
+    'directory': 'server/api'
   },
   {
-    "project": "meepo",
-    "name": "meepo",
-    "branch": {
-      "dev": "master",
-      "production": "4.0.0"
+    'project': 'meepo',
+    'name': 'meepo',
+    'branch': {
+      'dev': 'master',
+      'production': '4.0.0'
     },
-    "git": "git@github.com:unitedstack/meepo.git",
-    "directory": "server/drivers"
+    'git': 'git@github.com:unitedstack/meepo.git',
+    'directory': 'server/drivers'
   },
   {
-    "project": "brewmaster",
-    "name": "brewmaster",
-    "branch": {
-      "dev": "master",
-      "production": "4.0.1"
+    'project': 'brewmaster',
+    'name': 'brewmaster',
+    'branch': {
+      'dev': 'master',
+      'production': '4.0.1'
     },
-    "git": "git@github.com:unitedstack/brewmaster.git",
-    "directory": "server/api"
+    'git': 'git@github.com:unitedstack/brewmaster.git',
+    'directory': 'server/api'
   },
   {
-    "name": "invoker",
-    "git": "git@github.com:unitedstack/invoker.git",
-    "directory": "server/api"
+    'name': 'invoker',
+    'git': 'git@github.com:unitedstack/invoker.git',
+    'directory': 'server/api'
   },
   {
-    "name": "tiny",
-    "git": "git@github.com:unitedstack/tiny.git",
-    "directory": "server/api"
+    'name': 'tiny',
+    'git': 'git@github.com:unitedstack/tiny.git',
+    'directory': 'server/api'
   },
   {
-    "name": "ticket",
-    "git": "git@github.com:unitedstack/doom.git",
-    "directory": "client/applications"
+    'name': 'ticket',
+    'git': 'git@github.com:unitedstack/doom.git',
+    'directory': 'client/applications'
   },
   {
-    "name": "bill",
-    "git": "git@github.com:unitedstack/viper.git",
-    "directory": "client/applications"
+    'name': 'bill',
+    'git': 'git@github.com:unitedstack/viper.git',
+    'directory': 'client/applications'
   }
 ];
 
@@ -118,7 +118,7 @@ const path = require('path');
 cp.execSync('rm -rf ~/subitems');
 
 const itemFatherDir = path.join('~/subitems');
-for(let i = 0;i < items.length; i++) {
+for(let i = 0; i < items.length; i++) {
 
   let item = items[i];
   let itemDir = path.join(itemFatherDir, item.name);
