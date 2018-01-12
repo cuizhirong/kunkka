82f33649166e706166e2865b538b9123d4c76789
majiajun <majiajun@unitedstack.com>
2018-01-12 18:25:38 +0800

    add cluster mode
    Type: NF

--- index.js
+++ index.js
@@ -2,19 +2,60 @@
 
 'use strict';
 
-const pkg = require('package.json');
-const boot = require('boot');
-const http = require('http');
-const config = require('server/config');
+const cfork = require('cfork');
 
-const start = Date.now();
-const port = config('port') || 5678;
-const hostname = config('hostname') || '0.0.0.0';
-const app = boot();
+const path = require('path');
+const util = require('util');
+const config = require('config');
 
-console.log('%s booted in %dms - port: %s', pkg.name, (Date.now()) - start, port);
+const logger = require('server/middlewares/logger').logger;
 
-const server = http.createServer(app);
-server.listen(port, hostname);
+const bootPath = path.join(__dirname, 'server/boot');
+cfork({
+  exec: bootPath,
+  env: {
+    'NODE_PATH': 'server:.'
+  },
+  count: config('clusterCount') || 4
+})
+  .on('fork', worker => {
+    logger.info('[worker:%d] new worker start', worker.process.pid);
+  })
+  .on('disconnect', worker => {
+    logger.error('[master:%s] wroker:%s disconnect, exitedAfterDisconnect: %s, state: %s.',
+      process.pid, worker.process.pid, worker.exitedAfterDisconnect, worker.state);
+  })
+  .on('exit', (worker, code, signal) => {
+    const exitCode = worker.process.exitCode;
+    const err = new Error(util.format('worker %s died (code: %s, signal: %s, exitedAfterDisconnect: %s, state: %s)',
+      worker.process.pid, exitCode, signal, worker.exitedAfterDisconnect, worker.state));
+    err.name = 'WorkerDiedError';
+    logger.error('[master:%s] wroker exit: %s', process.pid, err.stack);
+  })
 
-module.exports = server;
+  // if you do not listen to this event
+  // cfork will output this message to stderr
+  .on('unexpectedExit', (worker, code, signal) => {
+    const exitCode = worker.process.exitCode;
+    const propertyName = worker.hasOwnProperty('exitedAfterDisconnect') ? 'exitedAfterDisconnect' : 'suicide';
+    const err = new Error(util.format('worker:%s died unexpected (code: %s, signal: %s, %s: %s, state: %s)',
+      worker.process.pid, exitCode, signal, propertyName, worker[propertyName], worker.state));
+    err.name = 'WorkerDiedUnexpectedError';
+    logger.error('[cfork:master:%s]  unexpected exit %s',
+      process.pid, err.stack);
+  })
+
+  // emit when reach refork times limit
+  .on('reachReforkLimit', () => {
+    logger.error('reachReforkLimit');
+  });
+
+// if you do not listen to this event
+// cfork will listen it and output the error message to stderr
+process.on('uncaughtException', err => {
+  logger.error('[cfork:master:%s] master uncaughtException: %s', process.pid, err.stack);
+  logger.error(err);
+  setTimeout(() => {
+    process.exit(1);
+  }, 10000);
+});
