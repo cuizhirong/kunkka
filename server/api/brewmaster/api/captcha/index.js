5a33747386e548dd0fc690e875e4833784d51da9
majiajun <majiajun@unitedstack.com>
2018-01-20 22:54:30 +0800

    fix memory leak and performance bug
    Type: BF

--- server/api/brewmaster/api/captcha/index.js
+++ server/api/brewmaster/api/captcha/index.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const ccap = require('ccap');
+const captcha = ccap({
+  width: 200, //set width,default is 256
+  height: 60, //set height,default is 60
+  offset: 30, //set text spacing,default is 40
+  quality: 50, //set pic quality,default is 50
+  fontsize: 45 //set font size,default is 57
+});
 
 class Captcha {
   constructor(app) {
@@ -8,14 +15,6 @@ class Captcha {
   }
 
   static getCaptcha(req, res) {
-    let captcha = ccap({
-      width: 200, //set width,default is 256
-      height: 60, //set height,default is 60
-      offset: 30, //set text spacing,default is 40
-      quality: 50, //set pic quality,default is 50
-      fontsize: 45 //set font size,default is 57
-    });
-
     let ary = captcha.get();
     req.session.captcha = ary[0];
     res.send(ary[1]);
