91fc4113bbcf047ff0265b48c40a54c799f553e1
hbshun <bb@bbyy.me>
2018-06-14 11:54:33 +0800

    bugfix: db_migration for quota approval regionId
    Change-Id: I8d48c52cd5acbf129cec14b08614ee82c81a86c1
    Type: BF

---
+++ server/db_migration/migrations/20180614032933-kunkka.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'quota_approves',
+      'region',
+      {
+        type: Sequelize.STRING
+      }
+    )
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
